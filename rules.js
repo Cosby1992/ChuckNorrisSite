2939dae1cf65ff25b6c881d4ef0de581220d39f5
Cosby1992 <Ande016j@edu.easj.dk>
2019-10-02 17:44:26 +0200

    functioning

--- rules.js
+++ rules.js
@@ -1,25 +1,27 @@
 
 
+let resultText = document.getElementById('result');
+var categories = [];
+
 // random joke at update
 fetch('https://api.chucknorris.io/jokes/random')
     .then(response => response.json())
     .then(data => {
         // Do what you want with your data
 
-        
         document.getElementById('random_joke').innerHTML = data.value;
     })
     .catch(err => {
         console.error('An error ocurred', err);
     });
 
-var categories = [];
+
     // fetch categories
 fetch('https://api.chucknorris.io/jokes/categories')
     .then(response => response.json())
     .then(data => {
         // Do what you want with your data
-        categories = data;
+        categories = [...data];
         let dropdown = document.getElementsByClassName('dropdown-content')[0];
         for(let i = 0; i < data.length; i++){
 
@@ -32,15 +34,17 @@ fetch('https://api.chucknorris.io/jokes/categories')
         console.error('An error ocurred', err);
     });
 
+    //get joke from category selection
     catfetch = cat => {
 
     fetch('https://api.chucknorris.io/jokes/random?category=' + categories[cat])
     .then(response => response.json())
     .then(data => {
     // Do what you want with your data
-
     
         document.getElementById('random_joke').innerHTML = data.value;
+
+        result.innerHTML = "";
     })
     .catch(err => {
         console.error('An error ocurred', err);
@@ -51,7 +55,7 @@ fetch('https://api.chucknorris.io/jokes/categories')
 
 
 
-
+//fetch and update on searchbutton click
 const search_func = () => {  
 
     let searchText = 'https://api.chucknorris.io/jokes/search?query=' + document.getElementById("inp_search").value;
@@ -62,11 +66,31 @@ const search_func = () => {
     .then(data => {
         // Do what you want with your data
 
+        let jokeindex = Math.floor(Math.random() * data.result.length);
+
+        result.innerHTML = "Results: " + data.result.length;
         
-        document.getElementById('random_joke').innerHTML = data.result[0].value;
+        document.getElementById('random_joke').innerHTML = data.result[jokeindex].value;
     })
     .catch(err => {
+        result.innerHTML = "No results from '" + document.getElementById("inp_search").value + "'";
         console.error('An error ocurred', err);
     });
 
+}
+
+randomfact = () => {
+    // random joke at update
+fetch('https://api.chucknorris.io/jokes/random')
+.then(response => response.json())
+.then(data => {
+    // Do what you want with your data
+
+    document.getElementById('random_joke').innerHTML = data.value;
+
+    result.innerHTML = "";
+})
+.catch(err => {
+    console.error('An error ocurred', err);
+});
 }
